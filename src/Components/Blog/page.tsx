db2087b7f3f32c4f044277a37e1fba0f7630d71d
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-11-14 02:57:04 +0200

    Blog page Done

--- src/Components/Blog/page.tsx
+++ src/Components/Blog/page.tsx
@@ -1,15 +1,19 @@
 export default function Blog() {
-    return (
-
-        <>
-        <h2 className="text-4xl text-blue-500 font-semibold mb-8 text-center">Blog</h2>
-                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-                          <h3 className="text-2xl text-blue-400 font-semibold mb-4">Why I Love TypeScript</h3>
-                          <p className="text-lg mb-4">In this post, I'll discuss why TypeScript has become my go-to language for building robust and maintainable web applications.</p>
-                          <a href="/blog/why-i-love-typescript" className="text-blue-500">Read more</a>
-                        </div>
-                      </div>
-        </>
-    );
+  return (
+    <>
+      <h2 className="text-4xl text-neonGreen font-pixel mb-8 text-center">My Blog</h2>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="bg-gray-900 p-6 rounded-lg border border-neonGreen shadow-lg transition duration-300 transform hover:scale-105">
+          <h3 className="text-2xl text-neonGreen font-pixel font-semibold mb-4">Why I Love TypeScript</h3>
+          <p className="text-lg text-gray-300 font-vt323 mb-4">
+            Let's talk about why Typescript is so good.</p>
+          <a href="#" className="inline-block mt-4 px-6 py-2 bg-neonGreen text-gray-900 rounded-lg font-pixel hover:bg-blue-500 transition duration-300">
+            Read more
+          </a>
+        </div>
+        
+      </div>
+    </>
+  );
 }
