829e5d4956bc3d78a1973474ce1677dfccc74ad3
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-11-14 04:20:26 +0200

    home page refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { useState } from 'react';
 import About from '../Components/About/page';
 import Projects from '@/Components/Projects/page';
@@ -19,66 +19,63 @@ export default function Home() {
     if (!openWindows.includes(section)) {
       setOpenWindows([...openWindows, section]);
 
-      // if on a mobile device, set the position to center
+      // Set default window size and position based on screen width
       let newPosition = { top: 30, left: 320 };
-      let size = { width: 80, height: 60 };
-      let offset = 30; 
+      let newSize = { width: 80, height: 60 };
+      let offset = 30;
 
+      // Adjust position for smaller screens (Mobile View)
       if (window.innerWidth < 768) {
         newPosition = { top: 0, left: 0 };
-        size = { width: 100, height: 100 };
+        newSize = { width: 90, height: 90 };
         offset = 0;
       }
 
+      // Prevent overlap and adjust position for multiple windows
       const existingPositions = Object.values(positions);
       if (existingPositions.length > 0) {
         const lastPosition = existingPositions[existingPositions.length - 1];
-        newPosition.top = lastPosition.top + offset; 
-        newPosition.left = lastPosition.left + offset; 
+        newPosition.top = lastPosition.top + offset;
+        newPosition.left = lastPosition.left + offset;
       }
 
       setPositions((prevPositions) => ({
         ...prevPositions,
-        [section]: newPosition, 
+        [section]: newPosition,
       }));
 
-      setSize((prevSize) => ({ 
+      setSize((prevSize) => ({
         ...prevSize,
-        [section]: size,
+        [section]: newSize,
       }));
     }
   };
 
   const handleWindowClose = (section: string) => {
-    setOpenWindows(openWindows.filter(window => window !== section));
+    setOpenWindows(openWindows.filter((window) => window !== section));
     setPositions((prevPositions) => {
-      const { [section]: _, ...rest } = prevPositions; 
+      const { [section]: _, ...rest } = prevPositions;
       return rest;
     });
   };
 
   const handleWindowMaximize = (section: string) => {
-      // make the size 100vw and 100 vh
-      setPositions((prevPositions) => ({
-        ...prevPositions,
-        [section]: { top: 0, left: 0 },
+    setPositions((prevPositions) => ({
+      ...prevPositions,
+      [section]: { top: 0, left: 0 },
+    }));
+    if (size[section]?.width === 100) {
+      setSize((prevSize) => ({
+        ...prevSize,
+        [section]: { width: 80, height: 60 },
       }));
-      if(size[section].width === 100)
-        {
-          setSize((prevSize) => ({
-            ...prevSize,
-            [section]: { width: 80, height: 60 },
-          }));
-        }
-      else
-      {
-        setSize((prevSize) => ({
-          ...prevSize,
-          [section]: { width: 100, height: 100 },
-        }));
-      }
-
-  }
+    } else {
+      setSize((prevSize) => ({
+        ...prevSize,
+        [section]: { width: 100, height: 100 },
+      }));
+    }
+  };
 
   const handleDrag = (e: React.MouseEvent, windowName: string) => {
     const startX = e.clientX;
@@ -106,178 +103,66 @@ export default function Home() {
   };
 
   return (
-    
-<div className="bg-[#121212] text-white h-screen flex justify-center items-center overflow-y-clip">
-  {/* Desktop Icons */}
-  <div className="fixed top-0 left-0 h-full font-pixel grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2 p-4 z-50 overflow-y-auto">
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('about')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-user text-4xl text-neonGreen"></i>
+    <div className="bg-[#121212] text-white h-screen flex justify-center items-center overflow-y-clip">
+      {/* Desktop Icons */}
+      <div className="fixed top-0 left-0 h-full font-pixel grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-2 gap-4 p-4 z-50 overflow-y-auto">
+        {/* Each window icon */}
+        {['about', 'projects', 'skills', 'contact', 'resume', 'blog', 'Trash', 'Game'].map((section) => (
+          <div
+            key={section}
+            className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
+            onClick={() => handleWindowOpen(section)}
+          >
+            <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
+              <i className={`fas fa-${section === 'about' ? 'user' : section === 'projects' ? 'briefcase' : section === 'skills' ? 'laptop-code' : section === 'contact' ? 'envelope' : section === 'resume' ? 'file' : section === 'blog' ? 'comments' : section === 'Trash' ? 'trash' : 'gamepad'} text-4xl text-neonGreen`}></i>
+            </div>
+            <p className="text-sm mt-2 text-center transition duration-300">{section.charAt(0).toUpperCase() + section.slice(1)}</p>
+          </div>
+        ))}
       </div>
-      <p className="text-sm mt-2 text-center transition duration-300">About</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('projects')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-briefcase text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm  mt-2 text-center transition duration-300">Projects</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('skills')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-laptop-code text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Skills</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('contact')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-envelope text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Contact</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('resume')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-file text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Resume</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('blog')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-comments text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Blog</p>
-    </div>
-
-    <div 
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('Trash')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-trash text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Trash</p>
-    </div>
-
-    <div
-      className="flex flex-col items-center cursor-pointer transform transition duration-300 ease-in-out hover:scale-110"
-      onClick={() => handleWindowOpen('Game')}
-    >
-      <div className="p-4 rounded-lg shadow-md hover:border-neonGreen hover:border transition duration-300 ease-in-out">
-        <i className="fas fa-gamepad text-4xl text-neonGreen"></i>
-      </div>
-      <p className="text-sm mt-2 text-center transition duration-300">Game</p>
-    </div>
-  </div>
 
       <div className="relative w-full h-full flex items-center justify-center">
         {openWindows.map((window, index) => (
           <div
             key={index}
-            className="bg-forG rounded-lg border-2 border-neonGreen absolute w-96 min-h-[70vh] transition-all duration-200"
+            className="bg-forG rounded-lg border-2 border-neonGreen absolute transition-all duration-200"
             style={{
               top: `${positions[window]?.top}px`,
               left: `${positions[window]?.left}px`,
               width: `${size[window]?.width}vw`,
-              minHeight: `${size[window]?.height}vh`, 
+              minHeight: `${size[window]?.height}vh`,
               zIndex: 50 + index,
-            }} 
+            }}
           >
-            
-
             <div
               className="bg-[#121212] text-white p-2 rounded-t-lg flex justify-between items-center shadow-md border-b-2 border-neonGreen"
-              onMouseDown={(e) => handleDrag(e, window)} 
+              onMouseDown={(e) => handleDrag(e, window)}
             >
               <div className="flex gap-2">
-                {/* Buttons (Minimize, Maximize, Close) */}
-                <div className="w-3 h-3 bg-[#4CAF50] rounded-full hover:bg-[#45a049] cursor-pointer"
+                <div
+                  className="w-3 h-3 bg-[#4CAF50] rounded-full hover:bg-[#45a049] cursor-pointer"
                   onClick={() => handleWindowClose(window)}
-                  
-                ></div> {/* Minimize */}
-                <div className="w-3 h-3 bg-[#FFEB3B] rounded-full hover:bg-[#FFEB3B] cursor-pointer"
-                onClick={() => handleWindowMaximize(window)}
-                
-                ></div> {/* Maximize */}
+                ></div>
+                <div
+                  className="w-3 h-3 bg-[#FFEB3B] rounded-full hover:bg-[#FFEB3B] cursor-pointer"
+                  onClick={() => handleWindowMaximize(window)}
+                ></div>
                 <div
                   className="w-3 h-3 bg-[#F44336] rounded-full hover:bg-[#e53935] cursor-pointer"
                   onClick={() => handleWindowClose(window)}
-                ></div> 
+                ></div>
               </div>
-              <span className=" text-lg font-pixel text-retroPink">{window.charAt(0).toUpperCase() + window.slice(1)}</span>
+              <span className="text-lg font-pixel text-retroPink">{window.charAt(0).toUpperCase() + window.slice(1)}</span>
             </div>
 
-         
-            {window === 'about' && (
-              <section id = 'about' className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                <About />
-              </section>
-            )}
-
-            {window === 'projects' && (
-              <section id="projects" className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                <Projects />
-              </section>
-            )}
-
-
-            {window === 'skills' && (
-                  <section id="skills" className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                    <Skills />
-                  </section>
-            )}
-
-
-            {window === 'contact' && (
-                <section id="contact" className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                    <Contact />
-                  </section> 
-            )}
-
-            {window === 'resume' && (
-                <section id="resume" className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                  <Resume />                  
-                </section>
-            )}
-            {window === 'blog' && (
-              <section id="blog" className="p-6 text-white max-h-[90vh] overflow-y-auto">
-                      <Blog />
-              </section>
-            )}
-            
-            {window === 'Trash' && (
-              <section id="Trash" className="p-6 text-white rounded-lg shadow-lg max-h-[90vh] overflow-y-auto">
-                <Trash />
-              </section>
-            )}
-
-            {window === 'Game' && (
-              <section id="Game" className="p-6 text-white rounded-lg shadow-lg max-h-[90vh] overflow-y-auto">
-                <Game />
-              </section>
-            )}
-
+            {window === 'about' && <About />}
+            {window === 'projects' && <Projects />}
+            {window === 'skills' && <Skills />}
+            {window === 'contact' && <Contact />}
+            {window === 'resume' && <Resume />}
+            {window === 'blog' && <Blog />}
+            {window === 'Trash' && <Trash />}
+            {window === 'Game' && <Game />}
           </div>
         ))}
       </div>
